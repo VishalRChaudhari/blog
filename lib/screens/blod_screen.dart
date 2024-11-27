ca1a94a3a650dafd8ca727d60a0f70ce58956279
VishalRChaudhari <vishalchaudhari7800@gmail.com>
2024-11-27 16:11:57 +0530

    dependencies and updates

---
+++ lib/screens/blod_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class BlogTab extends StatefulWidget {
+  const BlogTab({super.key});
+
+  @override
+  State<BlogTab> createState() => _BlogTabState();
+}
+
+class _BlogTabState extends State<BlogTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
