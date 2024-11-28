41b402219ad1e9e5314116ee9d690b036b3e8aef
VishalRChaudhari <vishalchaudhari7800@gmail.com>
2024-11-28 19:50:34 +0530

    model

---
+++ lib/models/blog.dart
@@ -0,0 +1,14 @@
+class Blog {
+  Blog({
+    required this.title,
+    required this.deeplink,
+    required this.imageUrl,
+    required this.summary,
+    required this.content,
+  });
+  String title;
+  String deeplink;
+  String imageUrl;
+  String summary;
+  String content;
+}
