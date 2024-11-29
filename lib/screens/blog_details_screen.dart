4fa27c821a2d07ddad11c92a3175f6baaf1544bc
VishalRChaudhari <vishalchaudhari7800@gmail.com>
2024-11-29 16:19:10 +0530

    update in routng

--- lib/screens/blog_details_screen.dart
+++ lib/screens/blog_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:zarity_project/providers/firebase_provider.dart';
 
 class BlogDetailsScreen extends StatefulWidget {
   const BlogDetailsScreen({super.key});
@@ -10,6 +12,10 @@ class BlogDetailsScreen extends StatefulWidget {
 class _BlogDetailsScreenState extends State<BlogDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    final firebaseProvider = Provider.of<FirebaseProvider>(context);
+    if (firebaseProvider.data.isEmpty && !firebaseProvider.isLoading) {
+      firebaseProvider.fetchData();
+    }
     return Scaffold(
       appBar: AppBar(),
     );
--- lib/screens/blog_screen.dart
+++ lib/screens/blog_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:zarity_project/providers/firebase_provider.dart';
+import 'package:zarity_project/screens/blog_details_screen.dart';
 
 class BlogTab extends StatelessWidget {
   const BlogTab({super.key});
@@ -30,18 +31,25 @@ class BlogTab extends StatelessWidget {
                   itemBuilder: (context, index) {
                     final blog = firebaseProvider.data[index];
                     return ExpansionTile(
-                      trailing: IconButton(onPressed: (){
-
-                      }, icon: const Icon(Icons.navigate_next)),
+                      trailing: IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const BlogDetailsScreen(),
+                              ),
+                            );
+                          },
+                          icon: const Icon(Icons.navigate_next)),
                       showTrailingIcon: true,
-                      childrenPadding:const EdgeInsets.only(
-                              left: 14, bottom: 8,right: 7), 
+                      childrenPadding:
+                          const EdgeInsets.only(left: 14, bottom: 8, right: 7),
                       title: Text(blog.title),
                       children: [
-                         Divider(
-                           color: Colors.grey[400],
-                           thickness: 0.5,
-                         ),
+                        Divider(
+                          color: Colors.grey[400],
+                          thickness: 0.5,
+                        ),
                         Text(blog.summary),
                       ],
                     );
@@ -50,33 +58,3 @@ class BlogTab extends StatelessWidget {
     );
   }
 }
-/*
-ListView.builder(
-        itemCount: firebaseProvider.data.length,
-        itemBuilder: (context, index) {
-          final blog = firebaseProvider.data[index];
-          return ExpansionTile(title: Text(blog.title));
-        },
-      ),
-
- FutureBuilder<List<Blog>>(
-        future: FirebaseProvider().fetchData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No Blogs available'));
-          }
-          final blogs = firebaseProvider.data;
-          return ListView.builder(
-            itemCount: blogs.length,
-            itemBuilder: (context, index) {
-              final blog = blogs[index];
-              return ExpansionTile(title: Text(blog.title));
-            },
-          );
-        },
-      ),
-      */
